41a4486b4097f2e35e69d21f68c665188ca578f5
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2025-04-30 00:22:01 +0000

    Using structured output

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -12,8 +12,41 @@ class MacrosController < ApplicationController
     c = OpenAI::Chat.new
     c.system("You are an expert nutritionist. Estimate the macronutrients (carbohydrates, protein, and fat) in grams, as well as total calories in kcal.")
     c.user(@the_description, image: @the_image)
-    @result = c.assistant!
 
+    c.schema = '{
+      "name": "nutrition_info",
+      "schema": {
+        "type": "object",
+        "properties": {
+          "carbohydrates": {
+            "type": "integer",
+            "description": "Amount of carbohydrates in grams."
+          },
+          "protein": {
+            "type": "integer",
+            "description": "Amount of protein in grams."
+          },
+          "fat": {
+            "type": "integer",
+            "description": "Amount of fat in grams."
+          },
+          "total_calories": {
+            "type": "integer",
+            "description": "Total calories in kilocalories."
+          }
+        },
+        "required": [
+          "carbohydrates",
+          "protein",
+          "fat",
+          "total_calories"
+        ],
+        "additionalProperties": false
+      },
+      "strict": true
+    }'
+
+    @result = c.assistant!
 
     render({ :template => "macro_templates/results" })
   end
